84673a582a7e5801e2e18eb5eb2a950da8190c7f
Levitskyi <taras120591@mail.ru>
2013-10-16 16:33:49 +0300

    1-st commit, create project

---
+++ myjs.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+	
+	$('.stage').animate({
+		top:'120px'
+	},300);
+	$('.stage').animate({
+		top:'60px'
+	},300);
+	$('.stage').animate({
+		top:'120px'
+	},270);
+	$('.stage').animate({
+		top:'90px'
+	},220);
+	$('.stage').animate({
+		top:'120px'
+	},200);
+	$('.stage').animate({
+		top:'115px'
+	},150);
+	$('.stage').animate({
+		top:'120px'
+	},100);
+	$('#some_block').animate({
+		width:'250px'
+	},1000);
+});//end ready
